ae822efd13c936501681cc6f4229f4b0fe962f24
Matt Taggart <mtaggart89@gmail.com>
2026-01-03 15:07:44 -0700

    get basic app design working

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// Learn more: https://docs.expo.dev/guides/metro-config/ and https://www.nativewind.dev/quick-starts/expo
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: './global.css' });
